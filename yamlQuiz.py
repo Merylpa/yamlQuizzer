54110ad438757336a039b0bbf9aec10e8fde0e6f
Meryl Patrick <meno@Meryls-Air.fios-router.home>
2020-07-11 13:49:26 -0400

    simplified yaml format

--- yamlQuiz.py
+++ yamlQuiz.py
@@ -2,15 +2,6 @@ import yaml
 import random
 import sys
 
-def get_flashcards(sections):
-    """
-    Extract terms from sections
-    """
-    flashcards= []
-    for section in sections:
-        for subsection in section['sections']:
-            flashcards += subsection.get('terms', [])
-    return flashcards
 
 if len(sys.argv) < 2:
     print("Provide input files for flashcard generation")
@@ -20,7 +11,7 @@ deck = []
 for filein in sys.argv[1:]:
     with open(filein) as stream:
         parsed = yaml.load(stream, Loader=yaml.FullLoader)
-        deck += get_flashcards(parsed)
+        deck += parsed
 
 fullDeck = len(deck)
 print("\t 'q' to Quit, enter to continue")
